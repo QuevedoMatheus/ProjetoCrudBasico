d60defa7ee5183c2beef98ba5093c218ee679fe3
unknown <matheusquevedobio@gmail.com>
2024-01-14 19:42:35 -0300

    Iniciando projeto teste de crud completo

---
+++ CRUD/src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,79 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class UserDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant brithDate;
+	private Integer children;
+	
+	public UserDTO() {
+		
+	}
+
+	public UserDTO(Long id, String name, String cpf, Double income, Instant brithDate, Integer children) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.brithDate = brithDate;
+		this.children = children;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBrithDate() {
+		return brithDate;
+	}
+
+	public void setBrithDate(Instant brithDate) {
+		this.brithDate = brithDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+}
---
+++ CRUD/src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,116 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@Column(unique = true)
+	private String cpf;
+	
+	private Double income;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant brithDate;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Integer children;
+	
+	public User () {
+		
+	}
+
+	public User(Long id, String name, String cpf, double income, Instant brithDate, Integer children) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.brithDate = brithDate;
+		this.children = children;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getcpf() {
+		return cpf;
+	}
+
+	public void setcpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public double getIncome() {
+		return income;
+	}
+
+	public void setIncome(double income) {
+		this.income = income;
+	}
+
+	public Instant getBrithDate() {
+		return brithDate;
+	}
+
+	public void setBrithDate(Instant brithDate) {
+		this.brithDate = brithDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
